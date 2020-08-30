96aced52581af81086a2676a07a04da4bef761ca
kostyayakimovich <yakimovichkostya@gmail.com>
2020-08-31 01:14:50 +0300

    feat: create basic burger-menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { useCallback, useState, useEffect } from 'react';
 import Burger from '../../assets/images/Burger-menu-icon.png';
 import BurgerClose from '../../assets/images/Burger-menu-close.png';
+import Chevron from "../../assets/images/chevron.png";
 import { itemsMenu } from './menuData';
 import HoverMenu from "../HoverMenu";
 import "./style.scss";
@@ -9,23 +10,23 @@ const Header = () => {
   const [isElementHasChild, setIsElementHasChild] = useState(false);
   const [secondChildElementsMenu, setSecondChildElementsMenu] = useState([]);
   const [isBurger, setIsBurger] = useState(true);
+  const [isChevronDown, setIsChevronDown] = useState(true);
   const mainElementsMenu = [];
-  const valueElementsMenu = [];
 
   for (let key in itemsMenu) {
-    mainElementsMenu.push(key)
-    valueElementsMenu.push(itemsMenu[key]);
+    mainElementsMenu.push(itemsMenu[key])
   }
+
   const checkMainElementMenu = useCallback((element) => {
     const firstValue = [];
     for (let key in itemsMenu) {
-      if (key === element && typeof (itemsMenu[key]) === "object") {
+      if (key === element && itemsMenu[key].children) {
         setIsElementHasChild(true);
         for (let firstChildKey in itemsMenu[key]) {
           firstValue.push((itemsMenu[key])[firstChildKey]);
         }
         setSecondChildElementsMenu(firstValue);
-      } else if (key === element && typeof (itemsMenu[key]) === "string") {
+      } else if (key === element && !itemsMenu[key].children) {
         setIsElementHasChild(false);
       }
     }
@@ -35,11 +36,20 @@ const Header = () => {
     isBurger ? setIsBurger(false) : setIsBurger(true)
   }, [isBurger]);
 
+  const handleClickChevron = useCallback((el) => {
+    const firstValue = [];
+    for (let key in itemsMenu) {
+      if (key === el) {
+        firstValue.push(itemsMenu[key].children)
+      }
+    }
+    isChevronDown ? setIsChevronDown(false) : setIsChevronDown(true);
+  }, [isChevronDown]);
+
   useEffect(() => {
   }, [isElementHasChild]);
 
   return (
-
     <>
       <header className="header">
         <div className="burger"  >
@@ -48,25 +58,57 @@ const Header = () => {
             id="imgBurger"
             src={isBurger ? Burger : BurgerClose}
             alt="icon burger"
-          />
-        </div>
+          /> </div>
+        {/*descktop*/}
         <nav className="header_navigation">
           <ul className="menu" id="menu">
             {mainElementsMenu.map(el => (
               <li className="item_menu"
-                key={el} onMouseOver={() => checkMainElementMenu(el)}
+                key={el.value} onMouseOver={() => checkMainElementMenu(el.value)}
               >
-                {el}
+                {el.value}
               </li>
             ))}
-
           </ul>
         </nav>
+        <h1 className="logo">OZITAG</h1>
       </header>
-      {isElementHasChild && <HoverMenu
+      {isElementHasChild && isBurger && <HoverMenu
         setIsElementHasChild={setIsElementHasChild}
         secondChildElementsMenu={secondChildElementsMenu}
       />}
+      {!isBurger && <nav className="burger_header_navigation">
+        <ul className="burger_menu" id="burger_menu">
+          {mainElementsMenu.map(el => (
+            <li className="burger_item_menu"
+              key={el.value}
+            >
+              <div className="burger_menu_block">
+                <div>{el.value}</div>
+
+                {el.children && <img className={isChevronDown ? "chevron_img" : "chevron_img_rotate"}
+                  onClick={() => handleClickChevron(el.value)}
+                  src={Chevron}
+                  alt="chevron"
+                />}</div>
+
+              {el.children ? el.children.map(item =>
+                <div className="burger_menu_block" key={item.value}>
+                  <div className="burger_first_child"> <p>{item.value}</p>
+                    {item.children ? item.children.map(itemCh => (
+                      <p key={itemCh} className="burger_second_child" >{itemCh}</p>
+                    )) : null}
+                  </div>
+                  {item.children && <img className={isChevronDown ? "chevron_img" : "chevron_img_rotate"}
+                    onClick={() => handleClickChevron(el.value)}
+                    src={Chevron}
+                    alt="chevron"
+                  />}
+                </div>) : null}
+            </li>
+          ))}
+        </ul>
+      </nav>}
     </>
 
   );
--- src/components/HoverMenu/HoverMenu.js
+++ src/components/HoverMenu/HoverMenu.js
@@ -2,26 +2,30 @@ import React from 'react';
 import "./style.scss";
 
 const HoverMenu = ({ setIsElementHasChild, secondChildElementsMenu }) => {
+
   return (
     <>
       <section className="hover_menu">
         <nav className="hover_menu_navigation" onMouseLeave={() => setIsElementHasChild(false)}>
           <ul className="menu" id="menu" >
-            {secondChildElementsMenu.map(el => {
-              return (
-                <li className="item_menu_hover"
-                  key={typeof (el) === "object" ? el.value : el}
-                >
-                  {typeof (el) === "object" ?
-                    el.children ? (
-                      [...el.value, ...el.children].map(item =>
-                        <p key={item} className="item_menu_hover">{item}</p>)) : <p>{el.value}</p>
-                    :
-                    <p className="item_menu_hover">{el}</p>}
-
-                </li>
-              )
-            })}
+            {secondChildElementsMenu.map(el => (
+              <li className="item_menu_hover"
+                key={el}
+              >
+                {Array.isArray(el) ? el.map(item =>
+                  item.children ?
+                    <div key={item.value} className="el_menu_hover">{item.value}
+                      {item.children.map(elChildren =>
+                        <p key={elChildren} className="el_menu_hover_child">{elChildren}</p>)}
+                    </div>
+                    : <div key={item.value} className="el_menu_hover">
+                      {item.value}
+                    </div>)
+                  : null
+                }
+              </li>
+            ))
+            }
           </ul>
         </nav>
       </section>
